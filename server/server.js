65dba5aba1a38d2d530eb8bc78314461a281b517
Serdar <ssimsekler@yahoo.com>
2018-02-14 01:33:09 +0000

    POST

--- server/server.js
+++ server/server.js
@@ -1,6 +1,33 @@
+const express = require('express');
+const bodyParser = require('body-parser');
 
-var {mongoose} = require('./db/mongoose.js');
+const { mongoose } = require('./db/mongoose.js');
+const { Todo } = require('./models/todo.js');
+const { User } = require('./models/user.js');
 
-var {Todo} = require('./models/todo.js');
-var {User} = require('./models/user.js');
+const port = process.env.PORT || 3000;
 
+var app = express();
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+    var todo = new Todo({
+        text: req.body.text
+    })
+    todo.save().then((doc) => {
+        console.log('Record saved', doc);
+        res.send(doc);
+    }, (err) => {
+        console.log(err);
+        res.status('400').send(err);
+    });
+    console.log('POST', req.body);
+});
+
+app.get('/todos', (req, res) => {
+    console.log('GET', req.body);
+});
+
+app.listen(port, () => {
+    console.log(`Node server started on port ${port}`);
+})
