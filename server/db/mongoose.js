1941e1b5ce25edeff6f97ef695740fd986377efe
Serdar <ssimsekler@yahoo.com>
2018-02-14 23:46:17 +0000

    Fix heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const url = (process.env.MONGODB_URI) ? encodeURIComponent(process.env.MONGODB_URI) : 'mongodb://localhost:27017';
+const url = process.env.MONGODB_URI || 'mongodb://localhost:27017';
 const dbName = 'ToDoApp';
 
 mongoose.Promise = global.Promise;
