ff8ebc9104280f8a388d1b1bb193e28d43e9b873
Serdar <ssimsekler@yahoo.com>
2018-02-14 14:06:12 +0000

    Query by ID

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { ObjectID } = require('mongodb');
 
 const { mongoose } = require('./db/mongoose.js');
 const { Todo } = require('./models/todo.js');
 const { User } = require('./models/user.js');
+
 
 const port = process.env.PORT || 3000;
 
@@ -29,6 +31,24 @@ app.get('/todos', (req, res) => {
     Todo.find().then((todos) => {
         res.send({ todos });
     }, (err) => {
+        res.status(400).send(err);
+    });
+
+});
+
+app.get('/todos/:id', (req, res) => {
+    // console.log('GET', req.body);
+    var id = req.params.id;
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send({error: 'ID not valid!'});
+    }
+
+    Todo.findById(id).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+        res.send({ todo });
+    }).catch((e) => {
         res.status(400).send(err);
     });
 
