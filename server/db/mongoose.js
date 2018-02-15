e0a354ce796b91b766c598b9dad103e1fd0bcc9c
Serdar <ssimsekler@yahoo.com>
2018-02-15 00:05:37 +0000

    Heroku fix

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
-const url = process.env.MONGODB_URI || 'mongodb://localhost:27017';
 const dbName = 'ToDoApp';
+const url = process.env.MONGODB_URI || `mongodb://localhost:27017/${dbName}`;
 
 mongoose.Promise = global.Promise;
-mongoose.connect(`${url}/${dbName}`)
+mongoose.connect(`${url}`)
     .then(() => {
         console.log(`URL: ${url}`, ' MongoDB connection established.');
     })
